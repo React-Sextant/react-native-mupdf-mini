9ded4c3d10a5d7bfc57db6cb363020550a80bb0e
罗坤 <1498710037@qq.com>
2019-07-01 18:26:15 +0800

    Initial commit

---
+++ android/src/main/java/com/github/react/sextant/RCTMuPdfView.java
@@ -0,0 +1,32 @@
+package com.github.react.sextant;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+public class RCTMuPdfView implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    // Deprecated as of RN 0.47.0
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        List<ViewManager> modules = new ArrayList<>();
+        modules.add(new RCTMuPdfManager(reactContext));
+        return modules;
+    }
+}
